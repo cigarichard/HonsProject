951823e82fac01ca077a11ebc585152f2c4f4d6b
cigarichard <yuan991114@gmail.com>
2022-04-06 04:11:35 +0100

    4.6

--- main.py
+++ main.py
@@ -12,64 +12,51 @@ def main():
 
 
     '''
-    plot
+    plotting algorithms generated paths
+    pathView.add has three argument: algorithm label, color and k for control point distance
     '''
+    # plotting algorithm generated paths
     pathView = PathView(problem, nums_appr)
-    # pathView.add("bezier", "b", 0.4)
-    # pathView.add("bezier", "y", 0.8)
-    # pathView.add("bezier", "g", 0.6)
-    # pathView.add("bezier", "c", 0.4)
-    # pathView.add("bezier", "m", 0.2)
     pathView.add("bspline", "b", 1)
     pathView.add("bspline", "y", 0.8)
     pathView.add("bspline", "g", 0.6)
     pathView.add("bspline", "c", 0.4)
     pathView.add("bspline", "m", 0.2)
-    # pathView.add("cubicspline", "c", 0.4)
-    # pathView.add("cubicspline", "y", 0.8)
-    # pathView.add("cubicspline", "g", 0.6)
-    # pathView.add("cubicspline", "c", 0.4)
-    # pathView.add("cubicspline", "m", 0.2)
-    # print("done")
     pathView.add_arrow()
     pathView.show()
 
-    # pathView = PathView(problem, nums_appr)
-    # # pathView.add("bezier", "b", 0.4)
-    # # pathView.add_multiarc("bezier", "r", 0.4)
-    # # pathView.add("cubicspline", "y", 0.4)
-    # # pathView.add_multiarc("cubicspline", "g", 0.4)
-    # # pathView.add_multiarc("bspline", "b", 1)
-    # # pathView.add_multiarc("bspline", "y", 0.8)
-    # # pathView.add_multiarc("bspline", "g", 0.6)
-    # # pathView.add_multiarc("bspline", "c", 0.4)
-    # # pathView.add_multiarc("bspline", "m", 0.2)
-    # pathView.add_multiarc("cubicspline", "b", 1)
-    # pathView.add_multiarc("cubicspline", "y", 0.8)
-    # pathView.add_multiarc("cubicspline", "g", 0.6)
-    # pathView.add_multiarc("cubicspline", "c", 0.4)
-    # pathView.add_multiarc("cubicspline", "m", 0.2)
-    # pathView.add_arrow()
-    # pathView.show()
-    #
-    # pathView = PathView(problem, nums_appr)
-    # pathView.add_curvature("bezier", "-b", 1)
-    # pathView.add_curvature("bezier", "-y", 0.8)
-    # pathView.add_curvature("bezier", "-g", 0.6)
-    # pathView.add_curvature("bezier", "-c", 0.4)
-    # pathView.add_curvature("bezier", "-m", 0.2)
-    # # pathView.add_curvature("bspline", "b", 1)
-    # # pathView.add_curvature("bspline", "y", 0.8)
-    # # pathView.add_curvature("bspline", "g", 0.6)
-    # # pathView.add_curvature("bspline", "c", 0.4)
-    # # pathView.add_curvature("bspline", "m", 0.2)
-    # # pathView.add_curvature("cubicspline", "c", 0.4)
-    # # pathView.add_curvature("cubicspline", "y", 0.8)
-    # # pathView.add_curvature("cubicspline", "g", 0.6)
-    # # pathView.add_curvature("cubicspline", "c", 0.4)
-    # # pathView.add_curvature("cubicspline", "m", 0.2)
-    # pathView.show()
 
+    '''
+    plotting arc based paths
+    pathView.add_multiarc has three argument: algorithm label, color and k for control point distance
+    '''
+    pathView = PathView(problem, nums_appr)
+    pathView.add_multiarc("cubicspline", "b", 1)
+    pathView.add_multiarc("cubicspline", "y", 0.8)
+    pathView.add_multiarc("cubicspline", "g", 0.6)
+    pathView.add_multiarc("cubicspline", "c", 0.4)
+    pathView.add_multiarc("cubicspline", "m", 0.2)
+    pathView.add_arrow()
+    pathView.show()
+
+
+    '''
+    plotting curvature against path length
+    pathView.add_curvature has three argument: algorithm label, color and k for control point distance
+    '''
+    pathView = PathView(problem, nums_appr)
+    pathView.add_curvature("bezier", "-b", 1)
+    pathView.add_curvature("bezier", "-y", 0.8)
+    pathView.add_curvature("bezier", "-g", 0.6)
+    pathView.add_curvature("bezier", "-c", 0.4)
+    pathView.add_curvature("bezier", "-m", 0.2)
+    pathView.show()
+
+
+    '''
+    calculating and compute the optimal value of k for given problem
+    pathView.compute_strain has two argument: algorithm label, color
+    '''
     pathView = PathView(problem, nums_appr)
     pathView.compute_strain("bezier", 'r')
     pathView.compute_strain("cubicspline", 'b')
